44b7eda28e5f1c156e2110ce82749bd865bf570b
gabu <gabujr337@gmail.com>
2025-12-30 02:01:30 +0300

    more on functions

---
+++ functions.py
@@ -0,0 +1,27 @@
+def area_of_circle(radius):
+    pi = 3.14
+    area = pi * radius * radius
+    return area
+
+
+sword_length = 1.0
+spear_length = 2.0
+
+sword_area = area_of_circle(sword_length)
+spear_area = area_of_circle(spear_length)
+
+print(f"sword length : {sword_length} meters")
+print(f"sword attack area: {area_of_circle(sword_length)} square meters")
+print(f"spear_length {spear_length} meters")
+print(f"spear attack area: {spear_area} square meters")
+
+
+damage_one = 2
+damage_two = 4
+damage_three = 3
+damage_four = -1
+damage_five = 10 
+damage_six = 5
+
+
+
