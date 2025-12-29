30e05d7f348e89073b0a911d8a2850bfc4b44f1a
gabu <gabujr337@gmail.com>
2025-12-30 01:20:19 +0300

    on variables

---
+++ bootdev.py
@@ -0,0 +1,11 @@
+sword_damage = 10
+player_health = 100
+health_after_attack = player_health - sword_damage
+
+# Don't touch this line
+print(f"Lollifred's health is: {player_health}")
+print(f"Lollifred's is hit by a sword for: {sword_damage} damage...")
+print(f"Lollifred's health is now: {health_after_attack}")
+
+
+print("Welcome to Fantasy quest")
---
+++ variables.py
@@ -0,0 +1,18 @@
+player_health = 1000
+armor_multiplier = 2
+
+armor_health = player_health * armor_multiplier
+print(armor_health)
+
+
+best_sword = "scimitar"
+print(best_sword)
+
+
+
+
+
+
+
+
+
