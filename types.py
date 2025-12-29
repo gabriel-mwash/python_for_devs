b50fd16c101e079e0d20a0b3f2cc11fe20ba60e0
gabu <gabujr337@gmail.com>
2025-12-30 01:46:42 +0300

    more types

---
+++ types.py
@@ -0,0 +1,45 @@
+# bolean - true, false; 1 or 0
+# numeric - int whole numbers, decimals - floating points
+# str - string of type str, sequence of characters
+# there are immutable types or primitive types
+
+
+name = "Trash Puppy"
+
+height = "6ft"
+
+print("My name is " + name + "and I am " + height + " tall!")
+
+print(f"My name is {name} and I am {height} tall!")
+
+
+name = "Yarl"
+age = 37
+race = "dwarf"
+
+print(f"{name} is {age} years and is a {race}")
+
+enemy = None
+print(enemy is not None)
+
+
+# dynamically typed - variables can change their types look below
+
+speed = 5
+speed = "fifty kilometers per hour"
+
+sentence_start = "You have "
+sentence_end = " health"
+
+player1_health = "1200"
+player2_health = "1100"
+
+print(sentence_start + player1_health + sentence_end)
+print(sentence_start + player2_health + sentence_end)
+
+
+sword_name, sword_damage, sword_length = "Excalibur", 10, 200
+
+
+
+
