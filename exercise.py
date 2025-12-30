fad4ad4e5e5a434f936d288254d281840b0864c2
gabu <gabujr337@gmail.com>
2025-12-31 01:47:17 +0300

    on object methods

--- exercise.py
+++ exercise.py
@@ -7,3 +7,15 @@ def average(num1: float, num2: float) -> float:
     """
     return (num1 + num2) / 2
 
+def total_occurences(s1: str, s2: str, ch: str) -> int:
+    """Precondition: len(ch) == 1
+    Return the total number of times that ch occurs in s1 and s2.
+
+    >>> total_occurences('color', 'yellow', 'l')
+    3
+    >>> total_occurences('red', 'blue', 'l')
+    1
+    >>> total_occurences('green', 'purple', 'b')
+    0
+    """
+    return s1.count(ch) + s2.count(ch)
