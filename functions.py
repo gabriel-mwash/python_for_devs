12bedf71ec1119e609152efdcfa6811131b1accb
gabu <gabujr337@gmail.com>
2025-12-30 03:02:05 +0300

    printf some

--- functions.py
+++ functions.py
@@ -20,8 +20,23 @@ damage_one = 2
 damage_two = 4
 damage_three = 3
 damage_four = -1
-damage_five = 10 
+damage_five = 10
 damage_six = 5
 
 
+def triple_attack(slash_one, slash_two, slash_three):
+    return slash_one + slash_two + slash_three
 
+
+result = triple_attack(2, damage_two, 89)
+print(f"Result = {result}")
+
+
+
+print("Getting dmage for", damage_one, damage_two, "and", damage_three, "....")
+print(triple_attack(damage_one, damage_two, damage_three), "points of damage dealt!")
+print("=========================================")
+
+print("Getting damage for", damage_four, damage_five, "and", damage_six, "...")
+print(triple_attack(damage_four, damage_five, damage_six), "points of damage dealt!")
+print("=====================================")
