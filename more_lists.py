9cb5731b3b4c23aab7d87e3d18aadbfbcb132866
gabu <gabujr337@gmail.com>
2026-01-01 00:44:28 +0300

    check this file. much wasn't understood

--- more_lists.py
+++ more_lists.py
@@ -40,3 +40,42 @@ width = 1
 while width < 8:
     print(" " * (7 - width), "T" * width, sep="")
     width += 1
+
+
+
+
+rat1 = [12, 9, 3, 10]
+rat2 = [5, 6, 7, 8]
+
+
+
+if rat1[0] > rat2[0] and rat1[-1] > rat2[-1]:
+    print("rat one weighed more than rat 2 on day 1.")
+    print("rat two became heavier thant rat 1")
+else:
+     print("rat 2 became heavier thatn rat 1")
+
+
+if rat1[0] > rat2[0]:
+    if rat1[-1] > rat2[-1]:
+        print("rat 1 remained heaveri than rat2. ")
+    else:
+        print("rat 2 beame heavier than rat 1.")
+else:
+    print("rat 2 became heavier than rat 1")
+
+
+
+
+
+
+rat_1_weight = 20
+rat_2_weight = 30
+
+week_count = 0
+while rat_1_weight <= (1.25 * rat_1_weight) :
+    rat_1_weight = 1.04 * rat_1_weight
+    week_count = week_count + 1
+
+print(week_count)
+    
