5d7ca80b5ea58220944f74c43a631a70deeb8540
gabu <gabujr337@gmail.com>
2025-12-31 23:55:55 +0300

    while loop

--- more_lists.py
+++ more_lists.py
@@ -30,3 +30,13 @@ for width in range(1, 8):
 for width in range(1, 8):
     print(' ' * (7 - width), "T" * width, sep="")
 
+
+width = 1
+while width < 8:
+    print("T" * width)
+    width += 1
+
+while = 1
+while width < 8:
+    print(" " * (7 - width), "T" * width, sep="")
+    width += 1
