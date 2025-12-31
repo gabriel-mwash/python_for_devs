3e451757ed70167979971fea87b10ab829e7f8bf
gabu <gabujr337@gmail.com>
2025-12-31 23:49:12 +0300

    on whiles and fors

--- more_lists.py
+++ more_lists.py
@@ -19,3 +19,8 @@ def remove_neg(num_list: List[float]) -> None:
     for item in num_list:
         if item < 0:
             num_list.remove(item)
+
+triangle_length = 7
+for i in list(range(0, triangle_length)):
+    print("T" * i)
+
