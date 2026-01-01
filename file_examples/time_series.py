1e42ffbee03707fe9e959312ab3eea6c31c0f204
gabu <gabujr337@gmail.com>
2026-01-01 22:56:20 +0300

    some changes

--- file_examples/time_series.py
+++ file_examples/time_series.py
@@ -1,5 +1,6 @@
 from typing import TextIO
 
+
 def skip_header(reader: TextIO) -> None:
     """ Skip the header in reader and return the first real piece of data.
     >>> infile = StringIO("Example\\n# Comment\\n Comment\\nData line\\n")
