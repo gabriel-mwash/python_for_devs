5bb4bae66c2ef9826c0eaab864329e9f580e6181
gabu <gabujr337@gmail.com>
2026-01-03 23:52:31 +0300

    some change

--- exercise/11/consistent_keys_check.py
+++ exercise/11/consistent_keys_check.py
@@ -32,3 +32,35 @@ def db_consistent(scientist_data: dict) -> dict:
                 return false
             else:
                 continue
+
+
+
+
+# - authors code
+def db_consistent2(dict_of_dict):
+    """ return whether all inner dictionaries in dict_of_dict contain the
+    same keys
+
+    >>> db_consistent2({"A": {1: "a", 2: "b"}, "B": {2: "c", 3: "d"}})
+    False
+    >>> db_consistent({"A": {1: "a", 2: "b"}, "B": {2: "c", 1: "d"}})
+    True
+    """
+
+    inner_keys_list = []
+    # build a list of keys
+    for key in dict_of_dict:
+        inner_keys = list(dict_of_dict[key].keys())
+        inner_keys.sort()
+        inner_keys_list.append(inner_keys)
+
+    for i in range(1, len(inner_keys_list)):
+        # if the no of keys is different
+        if len(inner_key_list[0]) != len(inner_key_list[i]):
+            return False
+        # if the keys don't match
+        for j in range(len(inner_keys_list[0])):
+            if inner_keys_list[0][j] != inner_keys_list[1][j]:
+                return False
+    return True
+    
